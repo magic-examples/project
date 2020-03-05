fba7dcd32eac614fdd70d6d3d952f6bd149ab260
jascha <jascha@jaeh.at>
2020-03-05 21:39:01 +0100

    app: remove vars, logotext, add title, description

--- src/app.mjs
+++ src/app.mjs
@@ -1,51 +1,10 @@
 export const state = {
-  logotext: '@magic-themes/project',
+  title: '@magic-examples/project',
+  description: '@magic project page example.',
 
-  branding: ['@', 'project'],
+  branding: ['@', 'magic'],
 
   menu: [
     { to: '/#installation', text: 'setup' },
   ],
-
-  vars: {
-    background: {
-      dark: '#212121',
-      light: '#e0e0e0',
-    },
-
-    link: {
-      dark: '#8f6ab4',
-      light: '#8f6ab4',
-
-      hover: {
-        dark: '#412162',
-        light: '#412162',
-      },
-    },
-
-    neutral: '#5a5a5a',
-
-    primary: {
-      dark: '#412162',
-      light: '#8f6ab4',
-      neutral: 'rebeccapurple',
-    },
-
-    text: {
-      dark: '#c4c4c4',
-      light: '#212121',
-    },
-
-    white: '#ffffff',
-
-    maxWidth: '1200px',
-
-    fadeDuration: '500ms',
-
-    widths: {
-      tablet: '500px',
-      laptop: '1024px',
-      desktop: '1600px',
-    },
-  },
 }
